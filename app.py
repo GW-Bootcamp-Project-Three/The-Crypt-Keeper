10ec0921158ea0466d9ec7c654a324c6552b48f1
Desiree Herschberger <citizendez@gmail.com>
2021-03-15 13:43:51 -0400

    statsmodel

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ warnings.filterwarnings('ignore')
 
 import numpy as np
 from scipy import stats
-# import statsmodels.api as sm
+import statsmodels.api as sm
 
 # import matplotlib.pyplot as plt
 # import matplotlib as mpl
