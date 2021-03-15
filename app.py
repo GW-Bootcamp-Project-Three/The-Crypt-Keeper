23a68445ca5946529f0abaa3766245c63ee971fc
Desiree Herschberger <citizendez@gmail.com>
2021-03-15 19:33:11 -0400

    fix

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ from plotly.tools import FigureFactory as FF
 from CK_dbFunctions import view_exists, get_dataframe_from_db
 
 # Imports added by TK for machine learning
-# from itertools import product
-# from datetime import datetime
-# import warnings
+from itertools import product
+from datetime import datetime
+import warnings
 
 
-# import numpy as np
-# from scipy import stats
-# import statsmodels.api as sm
+import numpy as np
+from scipy import stats
+import statsmodels.api as sm
 
 
 app = Flask(__name__)  
@@ -162,116 +162,115 @@ def ticker():
     return resp
 
 # Tom's price predictor
-# @app.route("/price-prediction-data", methods=['GET', 'POST'])
-# def price_prediction_data():
+@app.route("/price-prediction-data", methods=['GET', 'POST'])
+def price_prediction_data():
 
-#     coin_of_interest = request.form['CoinName']
-#     print('test'+coin_of_interest)
+    coin_of_interest = request.form['CoinName']
+    print('test'+coin_of_interest)
     
-#     #Connect to Amazon SQL
-#     conn = engine.connect()
+    #Connect to Amazon SQL
+    conn = engine.connect()
 
-#     # Grab coin prices
-#     query = '''
-#     SELECT 
-#         RecordDate,
-#         OpenPrice,
-#         High,
-#         Low,
-#         ClosingPrice,
-#         AdjClose,
-#         Volume,
-#         c.CoinName,
-#         cph.TokenName
-#     FROM
-#         CoinPriceHistory cph
-#     INNER JOIN Coins c
-#         ON cph.CoinID = c.CoinID
-#     ORDER BY
-#         RecordDate
-#     '''
-#     coin_raw = pd.read_sql(query, conn)
+    # Grab coin prices
+    query = '''
+    SELECT 
+        RecordDate,
+        OpenPrice,
+        High,
+        Low,
+        ClosingPrice,
+        AdjClose,
+        Volume,
+        c.CoinName,
+        cph.TokenName
+    FROM
+        CoinPriceHistory cph
+    INNER JOIN Coins c
+        ON cph.CoinID = c.CoinID
+    ORDER BY
+        RecordDate
+    '''
+    coin_raw = pd.read_sql(query, conn)
 
-#     # Grab coin names
-#     query = '''
-#     SELECT CoinName FROM Coins
-#     '''
-#     coin_names = pd.read_sql(query,conn)
+    # Grab coin names
+    query = '''
+    SELECT CoinName FROM Coins
+    '''
+    coin_names = pd.read_sql(query,conn)
 
-#     # NEED TO IMPLEMENT: LINK TO PRICE-PREDICT.HTML
-#     # Get coin-of-interest from user
-#     # coin_of_interest = request.form['coi']
+    # NEED TO IMPLEMENT: LINK TO PRICE-PREDICT.HTML
+    # Get coin-of-interest from user
+    # coin_of_interest = request.form['coi']
 
-#     # NEED TO IMPLMENT: PASS RESPONSE BACK TO PRICE-PREDICT.HTML
-#     # Determine if coin is in db
-#     # For now, we print it to the terminal.
-#     if coin_names[coin_names['CoinName']==coin_of_interest].any().bool():
-#       print("We got your coin!")
-#     else:
-#       print('We NO got your coin')
+    # NEED TO IMPLMENT: PASS RESPONSE BACK TO PRICE-PREDICT.HTML
+    # Determine if coin is in db
+    # For now, we print it to the terminal.
+    if coin_names[coin_names['CoinName']==coin_of_interest].any().bool():
+      print("We got your coin!")
+    else:
+      print('We NO got your coin')
 
-#     # Clean data
-#     coin_history = coin_raw[coin_raw['CoinName']==coin_of_interest]
-#     coin_history['RecordDate'] = pd.to_datetime(coin_history['RecordDate'], errors='coerce')
-#     coin_history.rename(columns={'RecordDate':'Timestamp','OpenPrice':'Open', 'ClosingPrice':'Close'}, inplace=True)
-#     coin_history.set_index('Timestamp', inplace=True)
-#     coin_history.drop(['AdjClose', 'Volume', 'CoinName', 'TokenName'], axis=1, inplace=True)
-#     coin_history = coin_history[coin_history['Close']!=0]  
+    # Clean data
+    coin_history = coin_raw[coin_raw['CoinName']==coin_of_interest]
+    coin_history['RecordDate'] = pd.to_datetime(coin_history['RecordDate'], errors='coerce')
+    coin_history.rename(columns={'RecordDate':'Timestamp','OpenPrice':'Open', 'ClosingPrice':'Close'}, inplace=True)
+    coin_history.set_index('Timestamp', inplace=True)
+    coin_history.drop(['AdjClose', 'Volume', 'CoinName', 'TokenName'], axis=1, inplace=True)
+    coin_history = coin_history[coin_history['Close']!=0]  
 
-#     # Transform data for ARIMA
-#     coin_history['Box'], lmbda = stats.boxcox(coin_history.Close)
-#     coin_history['BoxDiff'] = coin_history.Box - coin_history.Box.shift(12)
-#     coin_history['BoxDiff2'] = coin_history.BoxDiff - coin_history.BoxDiff.shift(1)
+    # Transform data for ARIMA
+    coin_history['Box'], lmbda = stats.boxcox(coin_history.Close)
+    coin_history['BoxDiff'] = coin_history.Box - coin_history.Box.shift(12)
+    coin_history['BoxDiff2'] = coin_history.BoxDiff - coin_history.BoxDiff.shift(1)
 
-#     # Optimize ARIMA Prediction
-#     Qs = range(0, 2)
-#     qs = range(0, 3)
-#     Ps = range(0, 3)
-#     ps = range(0, 3)
-#     D=1
-#     d=1
-#     parameters = product(ps, qs, Ps, Qs)
-#     parameters_list = list(parameters)
-#     results = []
-#     best_aic = float("inf")
-#     warnings.filterwarnings('ignore')
-#     for param in parameters_list:
-#         try:
-#             model=sm.tsa.statespace.SARIMAX(coin_history.Box, order=(param[0], d, param[1]), 
-#                                         seasonal_order=(param[2], D, param[3], 12)).fit(disp=-1)
-#         except:
-#             print('Data cannot be conditioned for ARIMA model.  Sorry!') # Need to send this back to user
-#     aic = model.aic
-#     if aic < best_aic:
-#         best_model = model
-#         best_aic = aic
-#         best_param = param
-#     results.append([param, model.aic])
+    # Optimize ARIMA Prediction
+    Qs = range(0, 2)
+    qs = range(0, 3)
+    Ps = range(0, 3)
+    ps = range(0, 3)
+    D=1
+    d=1
+    parameters = product(ps, qs, Ps, Qs)
+    parameters_list = list(parameters)
+    results = []
+    best_aic = float("inf")
+    warnings.filterwarnings('ignore')
+    for param in parameters_list:
+        try:
+            model=sm.tsa.statespace.SARIMAX(coin_history.Box, order=(param[0], d, param[1]), 
+                                        seasonal_order=(param[2], D, param[3], 12)).fit(disp=-1)
+        except:
+            print('Data cannot be conditioned for ARIMA model.  Sorry!') # Need to send this back to user
+    aic = model.aic
+    if aic < best_aic:
+        best_model = model
+        best_aic = aic
+        best_param = param
+    results.append([param, model.aic])
 
-#     #Generate Price Prediction Data
-#     def invboxcox(y,lmbda):
-#         if lmbda == 0:
-#             return(np.exp(y))
-#         else:
-#             return(np.exp(np.log(lmbda*y+1)/lmbda))
-#     coin_history_with_predictions = coin_history[['Close']]
-#     coin_history_with_predictions['Forecast'] = invboxcox(best_model.predict(start = 0, end=(len(coin_history_with_predictions)-1)), lmbda)
-#     prediction_dates = [datetime(2021, 4, 30), datetime(2021, 5, 31), datetime(2021, 6, 30), 
-#              datetime(2021, 7, 31), datetime(2021, 8, 31), datetime(2021, 9, 30), datetime(2021, 10, 31),
-#              datetime(2021, 11, 30), datetime(2021, 12, 31)]
-#     future = pd.DataFrame(index=prediction_dates, columns= coin_history.columns)
-#     future['Forecast'] = invboxcox(best_model.forecast(steps=len(future)), lmbda).tolist()
-#     coin_history_with_predictions = pd.concat([coin_history_with_predictions, future])
-#     coin_history_with_predictions['Coin'] = coin_of_interest
-#     graph = coin_history_with_predictions.reset_index().rename(columns={'index':'Date'})
-#     graph2 = graph[['Coin','Date', 'Close', 'Forecast']]
+    #Generate Price Prediction Data
+    def invboxcox(y,lmbda):
+        if lmbda == 0:
+            return(np.exp(y))
+        else:
+            return(np.exp(np.log(lmbda*y+1)/lmbda))
+    coin_history_with_predictions = coin_history[['Close']]
+    coin_history_with_predictions['Forecast'] = invboxcox(best_model.predict(start = 0, end=(len(coin_history_with_predictions)-1)), lmbda)
+    prediction_dates = [datetime(2021, 4, 30), datetime(2021, 5, 31), datetime(2021, 6, 30), 
+             datetime(2021, 7, 31), datetime(2021, 8, 31), datetime(2021, 9, 30), datetime(2021, 10, 31),
+             datetime(2021, 11, 30), datetime(2021, 12, 31)]
+    future = pd.DataFrame(index=prediction_dates, columns= coin_history.columns)
+    future['Forecast'] = invboxcox(best_model.forecast(steps=len(future)), lmbda).tolist()
+    coin_history_with_predictions = pd.concat([coin_history_with_predictions, future])
+    coin_history_with_predictions['Coin'] = coin_of_interest
+    graph = coin_history_with_predictions.reset_index().rename(columns={'index':'Date'})
+    graph2 = graph[['Coin','Date', 'Close', 'Forecast']]
+    # Return Price Prediction Data to Plotly
+    _json = coin_history_with_predictions.to_json(orient='records')
+    resp = make_response(_json)
+    resp.headers['content-type'] = 'application/json'
 
-#     # Return Price Prediction Data to Plotly
-#     _json = graph2.to_json(orient='records')
-#     resp = make_response(_json)
-#     resp.headers['content-type'] = 'application/json'
-
-#     return resp
+    return resp
 
 
 @app.route("/price-predict")
