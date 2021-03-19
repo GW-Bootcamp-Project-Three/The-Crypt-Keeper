db1f08a3d384d9b3709bc5681860ac4fd4fdbea0
Pappas <Billy.Pappas@mail.house.gov>
2021-03-19 11:35:00 -0400

    Test

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,59 @@
+var myMap = L.map("map", {
+    center: [37.697, -97.314],
+    zoom:4
+  });
+  
+  // Adding tile layer to the map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken:'pk.eyJ1Ijoid2pwYXBwYXMiLCJhIjoiY2traGpneHM1MDBqOTJ3cW5yODI5emUxNSJ9.N1ijsLc2UYQvliuaFtrRPw'
+  }).addTo(myMap);
+  
+  var gIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+  
+  url = 'ATMdata.json'
+    
+  // Grab the data with d3
+  d3.json(url, function(response) {
+  
+    // console.log(response);
+
+    var markers = L.markerClusterGroup();
+  
+    resp = Object.values(response);
+    console.log(resp);
+  
+    // Loop through data
+    for (var i = 0; i < resp.length; i++) {
+  
+      
+      // Set the data location property to a variable
+      var lat = resp[i].Latitude;
+      var lng = resp[i].Longitude;
+
+      if(lat && lng) {
+        markers.addLayer(L.marker([lat, lng],{icon: gIcon})
+        .bindPopup(`<h3>${resp[i].Address}</h3><hr>${(resp[i].Name)}<hr>${(resp[i].Hours)}`));
+      }
+  
+    //   console.log([lat, lng]);
+  
+    // Add our marker cluster layer to the map
+    myMap.addLayer(markers);
+  
+  }});
+
 // // var myMap = L.map("map", {
 // //     center: [39.8283, -98.5795],
 // //     zoom: 5
@@ -125,59 +181,3 @@
 //     addMarkers(data, map);
     
 // }
-
-var myMap = L.map("map", {
-    center: [37.697, -97.314],
-    zoom:4
-  });
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/light-v10",
-    accessToken:'pk.eyJ1Ijoid2pwYXBwYXMiLCJhIjoiY2traGpneHM1MDBqOTJ3cW5yODI5emUxNSJ9.N1ijsLc2UYQvliuaFtrRPw'
-  }).addTo(myMap);
-  
-  var gIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
-  
-  url = 'ATMdata.json'
-    
-  // Grab the data with d3
-  d3.json(url, function(response) {
-  
-    // console.log(response);
-
-    var markers = L.markerClusterGroup();
-  
-    resp = Object.values(response);
-    console.log(resp);
-  
-    // Loop through data
-    for (var i = 0; i < resp.length; i++) {
-  
-      
-      // Set the data location property to a variable
-      var lat = resp[i].Latitude;
-      var lng = resp[i].Longitude;
-
-      if(lat && lng) {
-        markers.addLayer(L.marker([lat, lng],{icon: gIcon})
-        .bindPopup(`<h3>${resp[i].Address}</h3><hr>${(resp[i].Name)}<hr>${(resp[i].Hours)}`));
-      }
-  
-    //   console.log([lat, lng]);
-  
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-  
-  }});
